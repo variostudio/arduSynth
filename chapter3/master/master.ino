32a5094519904db2849404842b8973884cbea4e9
troll <dima.pashko@gmail.com>
2018-02-22 22:08:09 +0200

    New versions of playing notes with I2C - bugfix

--- chapter3/master/master.ino
+++ chapter3/master/master.ino
@@ -1,22 +1,30 @@
 #include <Wire.h>
 
+byte note = 6;
+
 void setup() {
   Wire.begin(); // join i2c bus (address optional for master)
   randomSeed(analogRead(0));
 }
 
 void loop() {
-  Wire.beginTransmission(7); 
-  Wire.write(random(17*4, 20*4));  //Random freq      
+  Wire.beginTransmission(7);
+  Wire.write(note * 4); //Random freq
   Wire.write(random(0, 5));        //Random wave form, 0..4
-  Wire.endTransmission();    
+  Wire.endTransmission();
 
-  delay(random(100, 900));
+  delay(500);
 
-  Wire.beginTransmission(8); 
-  Wire.write(random(20*4, 60*4));         
-  Wire.write(random(0, 5));        //Random wave form, 0..4     
-  Wire.endTransmission();  
+  Wire.beginTransmission(7);
+  Wire.write(0);
+  Wire.write(1);        //Random wave form, 0..4
+  Wire.endTransmission();
 
-  delay(random(500, 900));
+  delay(1000);
+
+
+  note++;
+  if (note > 40) {
+    note = 6;
+  }
 }
--- chapter3/wireNote/wireNote.ino
+++ chapter3/wireNote/wireNote.ino
@@ -1,6 +1,7 @@
 #include <Wire.h>
 /******** Load AVR timer interrupt macros ********/
 #include <avr/interrupt.h>
+#include <util/atomic.h>
 #include "waveform.h"
 
 /******** Sine wave parameters ********/
@@ -8,52 +9,68 @@
 #define AMP     127      // Multiplication factor for the sine wave
 #define OFFSET  128      // Offset shifts wave to just positive values
 
+#define NORMAL 0;
+#define NEW_NOTE 1
+#define DECAY 2
+
 /******** Lookup table ********/
 #define LENGTH  256  // The length of the waveform lookup table
 byte wave[LENGTH];   // Storage for the waveform
+byte origwave[LENGTH];   // Storage for the waveform
 byte index = 0;
-int decayI;
-int decayJ;
+byte decayI;
+byte decayJ;
 
-boolean noteFinished = true;
+byte state = 0;
+byte note_code = 40 * 4;
+byte note_form = 0;
 
 /******** Populate the waveform lookup table with a sine wave ********/
-void initNote(int code, int form) {
-  noteFinished = false;
-  cli();
-  decayI = 0;
-  decayJ = 0;
-  index = 0;
-  OCR2A = code;             // sets the frequency of the generated wave
+void initNote() {
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+    decayI = 0;
+    decayJ = 0;
+    index = 0;
 
-  waveform(form);
+    TCCR1B  = (1 << CS10);    // Set prescaler to full 16MHz
+    TCCR1A |= (1 << COM1A1);  // PWM pin to go low when TCNT1=OCR1A
+    TCCR1A |= (1 << WGM10);   // Put timer into 8-bit fast PWM mode
+    TCCR1B |= (1 << WGM12);
 
-  sei(); 
+    /******** Set up timer 2 to call ISR ********/
+    TCCR2A = 0;               // We need no options in control register A
+    TCCR2B = (1 << CS20);     // Set prescaller to divide by 1
+    TIMSK2 = (1 << OCIE2A);   // Set timer to call ISR when TCNT2 = OCRA2
+
+    OCR2A = note_code;             // sets the frequency of the generated wave
+
+    memcpy(wave, origwave, LENGTH);
+
+    state = NORMAL;
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
 }
 
-void setup() {  
-  Wire.begin(8);                // join i2c bus with address #8
+void setup() {
+  waveform(note_form);
+  Wire.begin(7);                // join i2c bus with address #8
   Wire.onReceive(receiveEvent); // register event
-  initNote(32*4, SINE);
-  
+  initNote();
+  pinMode(LED_BUILTIN, OUTPUT);
+
   /******** Set timer1 for 8-bit fast PWM output ********/
   pinMode(9, OUTPUT);       // Make timer's PWM pin an output
-  
-  TCCR1B  = (1 << CS10);    // Set prescaler to full 16MHz
-  TCCR1A |= (1 << COM1A1);  // PWM pin to go low when TCNT1=OCR1A
-  TCCR1A |= (1 << WGM10);   // Put timer into 8-bit fast PWM mode
-  TCCR1B |= (1 << WGM12); 
 
-  /******** Set up timer 2 to call ISR ********/
-  TCCR2A = 0;               // We need no options in control register A
-  TCCR2B = (1 << CS20);     // Set prescaller to divide by 1
-  TIMSK2 = (1 << OCIE2A);   // Set timer to call ISR when TCNT2 = OCRA2
   sei();                    // Enable interrupts to generate waveform!
 }
 
 void loop()
-{  
-  decay();
+{
+  if (state == DECAY) {
+    decay();
+  } else if (state == NEW_NOTE) {
+    initNote();
+  }
 }
 
 /******** Called every time TCNT2 = OCR2A ********/
@@ -63,79 +80,93 @@ ISR(TIMER2_COMPA_vect) {  // Called each time TCNT2 == OCR2A
   TCNT2 = 6;              // Timing to compensate for time spent in ISR
 }
 
-  
+
 void decay() {
-  if (wave[decayI]<127) {
+  if (wave[decayI] < 127) {
     wave[decayI]++;
   }
-  if (wave[decayI]>127) {
+  if (wave[decayI] > 127) {
     wave[decayI]--;
   }
-    
-  if (decayI == LENGTH-1) {
+
+  if (decayI == LENGTH - 1) {
     decayI = 0;
     decayJ++;
     delay(2);
   } else {
-    decayI++;  
+    decayI++;
   }
   if (decayJ == 127) {
     decayJ = 0;
-    //cli();
-    noteFinished = true;
+    digitalWrite(LED_BUILTIN, LOW);
   }
 }
-  
+
 
 void receiveEvent(int howMany) {
-  int code = Wire.read();    // receive byte as an integer
-  int form = Wire.read();
+  if (Wire.available() > 1)  {
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+      note_code = Wire.read();    // receive byte as an integer
+      note_form = Wire.read();
 
-  if (code > 0) {
-    initNote(code, form);
+      if (note_code > 0) {
+        state = NEW_NOTE;
+      } else {
+        state = DECAY;
+      }
+    }
   }
 }
 
-void waveform(int w) {
- switch(w) {
+void waveform(byte w) {
+  /* switch(w) {
 
-   case SINE: 
-    for (int i=0; i<LENGTH; i++) 
-      {float v = OFFSET+(AMP*sin((PI2/LENGTH)*i));
-      wave[i]=int(v);
-    }
-    break;
-
-  case RAMP:
-    for (int i=0; i<LENGTH; i++) {
-      wave[i]=i;
-    }
-    break;
-
-  case TRIANGLE:
-    for (int i=0; i<LENGTH; i++) {
-      if (i<(LENGTH/2)) {
-        wave[i]=i*2;
-      } else {
-        wave[i]=(LENGTH-1)-(i*2);
+     case SINE:
+      for (int i=0; i<LENGTH; i++)
+        {float v = OFFSET+(AMP*sin((PI2/LENGTH)*i));
+        wave[i]=int(v);
       }
-    }
-    break;
-    
-  case SQUARE:
-    for (int i=0; i<(LENGTH/2); i++) {
-      wave[i]=255;
-    }
-    for (int i=(LENGTH/2)+1; i<LENGTH; i++) {
-      wave[i]=0;
-    }    
-    break;
+      break;
 
-  case RANDOM:
-    randomSeed(2);
-    for (int i=0; i<LENGTH; i++) {
-      wave[i]=random(255);
+    case RAMP:
+      for (int i=0; i<LENGTH; i++) {
+        wave[i]=i;
+      }
+      break;
+
+    case TRIANGLE:
+      for (int i=0; i<LENGTH; i++) {
+        if (i<(LENGTH/2)) {
+          wave[i]=i*2;
+        } else {
+          wave[i]=(LENGTH-1)-(i*2);
+        }
+      }
+      break;
+
+    case SQUARE:
+      for (int i=0; i<(LENGTH/2); i++) {
+        wave[i]=255;
+      }
+      for (int i=(LENGTH/2)+1; i<LENGTH; i++) {
+        wave[i]=0;
+      }
+      break;
+
+    case RANDOM:
+      randomSeed(2);
+      for (int i=0; i<LENGTH; i++) {
+        wave[i]=random(255);
+      }
+      break;
+    }*/
+
+  for (int i = 0; i < LENGTH; i++) {
+    if (i < (LENGTH / 2)) {
+      origwave[i] = i * 2;
+    } else {
+      origwave[i] = (LENGTH - 1) - (i * 2);
     }
-    break;
   }
+
 }
